e6265913abddda440681ff4f0879ae289aef5686
kambayashia <atsushi.kambayashi@gree.net>
2013-12-18 22:41:49 +0900

    changed first scene

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -1,7 +1,24 @@
 #include "AppDelegate.h"
-#include "StageScene.h"
+#include "Scene/StageScene.h"
+#include "HelloWorldScene.h"
+#include "TitleScene.h"
 
 USING_NS_CC;
+
+typedef struct tagResource
+{
+    cocos2d::CCSize size;
+    char directory[100];
+}Resource;
+/*
+static Resource smallResource  =  { cocos2d::CCSizeMake(480, 320),   "iphone" };
+static Resource mediumResource =  { cocos2d::CCSizeMake(1024, 768),  "ipad"   };
+static Resource largeResource  =  { cocos2d::CCSizeMake(2048, 1536), "ipadhd" };
+ */
+static Resource smallResource  =  { cocos2d::CCSizeMake(480, 320),   "hdpi" };
+static Resource mediumResource =  { cocos2d::CCSizeMake(1024, 768),  "hdpi"   };
+static Resource largeResource  =  { cocos2d::CCSizeMake(2048, 1536), "hdpi" };
+static cocos2d::CCSize designResolutionSize = cocos2d::CCSizeMake(480, 320);
 
 AppDelegate::AppDelegate() {
 
@@ -18,6 +35,27 @@ bool AppDelegate::applicationDidFinishLaunching() {
 
     director->setOpenGLView(eglView);
 	
+    eglView->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, kResolutionNoBorder);
+    CCSize frameSize = eglView->getFrameSize();
+    std::vector<std::string> searchPath;
+    
+    if (frameSize.height > mediumResource.size.height)
+    {
+        searchPath.push_back(largeResource.directory);
+        director->setContentScaleFactor(largeResource.size.height/designResolutionSize.height);
+    }
+    else if (frameSize.height > smallResource.size.height)
+    {
+        searchPath.push_back(mediumResource.directory);
+        director->setContentScaleFactor(mediumResource.size.height/designResolutionSize.height);
+    }
+    else
+    {
+        searchPath.push_back(smallResource.directory);
+        director->setContentScaleFactor(smallResource.size.height/designResolutionSize.height);
+    }
+    CCFileUtils::sharedFileUtils()->setSearchPaths(searchPath);
+    
     // turn on display FPS
     director->setDisplayStats(true);
 
@@ -25,7 +63,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     director->setAnimationInterval(1.0 / 60);
 
     // create a scene. it's an autorelease object
-    auto scene = Stage::createScene();
+    auto scene = Title::createScene();
 
     // run
     director->runWithScene(scene);
